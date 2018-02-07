468d088d3a51b7725361cecbc1d1f21e6401d8ce
nxj <1170288739@qq.com>
2018-02-07 10:00:47 +0800

    测试

--- src/main/java/com/stylefeng/guns/GunsApplication.java
+++ src/main/java/com/stylefeng/guns/GunsApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,8 +13,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  *
  * @author stylefeng
  * @Date 2017/5/21 12:06
+ * xxx
  */
-@SpringBootApplication
+@Spring
 public class GunsApplication extends WebMvcConfigurerAdapter{
 
     protected final static Logger logger = LoggerFactory.getLogger(GunsApplication.class);
